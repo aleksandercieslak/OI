8b1a891f65c2c127305397abe007fabc02531960
aleksandercieslak <cieslakolo@gmail.com>
2026-03-14 07:41:29 +0100

    Block Game

---
+++ problems/block-game.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using pii = pair<int,int>;
+using vi  = vector<int>;
+using vl  = vector<ll>;
+
+#define F first
+#define S second
+#define all(x)     (x).begin(),(x).end()
+#define pb         push_back
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+
+const int INF  = 1e9;
+const ll  LINF = 1e18;
+const int MOD  = 1e9+7;
+
+void solve() {
+    int numberOfBoards { 0 };
+    cin >> numberOfBoards;
+    vector<char> neededLetters(2000);
+    vector<int> numberOfLetters(26);
+    vector<pair<string, string>> boards(numberOfBoards);
+    for (int i = 0; i < numberOfBoards; i++) {
+        cin >> boards[i].first >> boards[i].second;
+    }
+    for (int i = 0; i < numberOfBoards; i++) {
+        set_union(boards[i].first.begin(), boards[i].first.end(), boards[i].second.begin(), boards[i].second.end(), back_inserter(neededLetters));
+    }
+    for (char i = 'a'; i <= 'z'; i++) {
+        numberOfLetters[i - 'a'] += count(neededLetters.begin(), neededLetters.end(), i);
+    }
+    for (int i = 0; i < 26; i++) {
+        cout << numberOfLetters[i] << "\n";
+    }
+    
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
