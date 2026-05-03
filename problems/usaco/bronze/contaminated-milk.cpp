d5dd249a5064625e04ea3a20be1ed23644efaac6
aleksandercieslak <cieslakolo@gmail.com>
2026-05-03 21:20:49 +0200

    Remove old template

--- problems/usaco/bronze/contaminated-milk.cpp
+++ problems/usaco/bronze/contaminated-milk.cpp
@@ -1,21 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll  = long long;
-using pii = pair<int,int>;
-using vi  = vector<int>;
-using vl  = vector<ll>;
-
-#define F first
-#define S second
-#define all(x)     (x).begin(),(x).end()
-#define pb         push_back
-#define rep(i,a,b) for(int i=(a);i<(b);i++)
-
-const int INF  = 1e9;
-const ll  LINF = 1e18;
-const int MOD  = 1e9+7;
-
 void solve() {
     int numberOfFriends { 0 };
     int numberOfTypes { 0 };
