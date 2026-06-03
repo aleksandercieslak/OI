5d6d9f9e122c11673412738f9eb777b2eb337384
aleksandercieslak <cieslakolo@gmail.com>
2026-06-03 14:48:53 +0200

    Maximum Median

---
+++ problems/usaco/silver/maximum-median.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int length { 0 };
+    int operations { 0 };
+    cin >> length >> operations;
+    vector<int> numbers (length);
+    for (int i = 0; i < length; i++) {
+        cin >> numbers[i];
+    }
+    sort(numbers.begin(), numbers.end());
+    int current { length/2 };
+    int members { 1 };
+    int median { numbers[current] };
+    while (operations >= (numbers[current+1]-numbers[current])*members && current < length) {
+        while (numbers[current] == numbers[current+1]) {
+            current++;
+            members++;
+        }
+        if (current >= length-1) { median += operations/members; break; }
+        median = numbers[current+1];
+        current++;
+        members++;
+        operations -= (numbers[current+1]-numbers[current])*members;
+    }
+    cout << median << "\n";
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
