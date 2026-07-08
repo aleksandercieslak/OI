007c21c8e189256e4c7c9c1dbf977573025133d3
aleksandercieslak <aleks.cieslak@proton.me>
2026-07-08 13:13:06 +0200

    Rectangular Pasture - rewrite

--- problems/usaco/silver/rectangular-pasture.cpp
+++ problems/usaco/silver/rectangular-pasture.cpp
@@ -9,42 +9,41 @@ void solve() {
         cin >> cows[i].first >> cows[i].second;
     }
     sort(cows.begin(), cows.end());
-    int index { 1 };
+    int newIndex { 1 };
     for (int i = 0; i < numberOfCows; i++) {
-        cows[i].first = index;
-        index++;
+        cows[i].first = newIndex;
+        newIndex++;
     }
-    sort(cows.begin(), cows.end(), [](pair<int, int> a, pair<int, int> b) {
-        return a.second < b.second;
-    });
-    index = 1;
+    sort(cows.begin(), cows.end(), [](pair<int, int> a, pair<int, int> b) { return a.second < b.second; });
+    newIndex = 1;
     for (int i = 0; i < numberOfCows; i++) {
-        cows[i].second = index;
-        index++;
+        cows[i].second = newIndex;
+        newIndex++;
     }
-    vector<vector<int>> position (numberOfCows+1, vector<int>(numberOfCows+1));
+    vector<vector<int>> field (numberOfCows+1, vector<int>(numberOfCows+1));
+    for (int i = 0; i < numberOfCows; i++) {
+        field[cows[i].first][cows[i].second] = 1;
+    }
     vector<vector<int>> prefix (numberOfCows+1, vector<int>(numberOfCows+1));
-    for (auto a : cows) {
-        position[a.first][a.second] = 1;
-    }
     for (int i = 1; i <= numberOfCows; i++) {
         for (int j = 1; j <= numberOfCows; j++) {
-            prefix[i][j] = position[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
+            prefix[i][j] = field[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
         }
     }
-    long long answer { 0 };
+    long long answer { 1 };
     for (int i = 0; i < numberOfCows; i++) {
         for (int j = i; j < numberOfCows; j++) {
-            int x1 { min(cows[i].first, cows[j].first)-1 };
-            int x2 { max(cows[i].first, cows[j].first)-1 };
-            int y1 { i };
-            int y2 { j };
-            int rec1 { prefix[x1+1][y2+1] - prefix[0][y2+1] - prefix[x1+1][y1] + prefix[0][y1] };
-            int rec2 { prefix[numberOfCows-1+1][y2+1] - prefix[x2][y2+1] - prefix[numberOfCows-1+1][y1] + prefix[x2][y1] };
-            answer += rec1 * rec2;
+            int left { min(cows[i].first, cows[j].first) };
+            int right { max(cows[i].first, cows[j].first) };
+            int bottom { min(cows[i].second, cows[j].second) };
+            int top { max(cows[i].second, cows[j].second) };
+            int leftPrefix { prefix[left-1][top] - prefix[left-1][bottom-1] };
+            leftPrefix += 1;
+            int rightPrefix { prefix[numberOfCows][top] - prefix[right][top] - prefix[numberOfCows][bottom-1] + prefix[right][bottom-1] };
+            rightPrefix += 1;
+            answer += rightPrefix*leftPrefix;
         }
     }
-    answer++;
     cout << answer << "\n";
 }
 
