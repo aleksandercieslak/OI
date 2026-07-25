3c8ed04ca6105a0519cb7196c8e3ffd2e74ac431
aleksandercieslak <aleks.cieslak@proton.me>
2026-07-25 17:32:16 +0200

    Bovine Acrobatics - rewrite

--- problems/usaco/silver/bovine-acrobatics.cpp
+++ problems/usaco/silver/bovine-acrobatics.cpp
@@ -3,36 +3,36 @@ using namespace std;
 
 void solve() {
     int numberOfCows { 0 };
-    int maxTowers { 0 };
-    int maxDiff { 0 };
-    cin >> numberOfCows >> maxTowers >> maxDiff;
+    int numberOfTowers { 0 };
+    int difference { 0 };
+    cin >> numberOfCows >> numberOfTowers >> difference;
     vector<pair<int, int>> cows (numberOfCows);
-    for (int i = 0; i < numberOfCows; i++) {
-        cin >> cows[i].first >> cows[i].second;
-        cows[i].second = min(cows[i].second, maxTowers);
+    for (auto& a : cows) {
+        cin >> a.first >> a.second;
+        a.second = min(a.second, numberOfTowers);
     }
     sort(cows.begin(), cows.end());
     reverse(cows.begin(), cows.end());
     deque<pair<int, int>> towers;
-    towers.push_back({INT_MAX, maxTowers});
-    int counter { 0 };
+    towers.push_back({INT_MAX, numberOfTowers});
+    int answer { 0 };
     for (int i = 0; i < numberOfCows; i++) {
         int temp { cows[i].second };
-        while (!towers.empty() && towers[0].first >= cows[i].first + maxDiff && temp > 0) {
-            if (towers[0].second <= temp) {
-                temp -= towers[0].second;
-                towers.pop_front();
-            }
-            else {
+        while (!towers.empty() && towers[0].first >= cows[i].first + difference && temp > 0) {
+            if (towers[0].second > temp) {
                 towers[0].second -= temp;
                 temp = 0;
                 break;
             }
+            else {
+                temp -= towers[0].second;
+                towers.pop_front();
+            }
         }
-        towers.push_back({cows[i].first, cows[i].second - temp});
-        counter += cows[i].second - temp;
+        towers.push_back({cows[i].first, cows[i].second-temp});
+        answer += cows[i].second-temp;
     }
-    cout << counter << "\n";
+    cout << answer << "\n";
 }
 
 int main() {
