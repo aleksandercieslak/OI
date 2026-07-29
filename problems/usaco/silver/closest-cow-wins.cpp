90f4fab32e452e6c845a5257f5926d340e138613
aleksandercieslak <aleks.cieslak@proton.me>
2026-07-29 14:50:06 +0200

    Closest Cow Wins - rewrite

--- problems/usaco/silver/closest-cow-wins.cpp
+++ problems/usaco/silver/closest-cow-wins.cpp
@@ -7,75 +7,70 @@ void solve() {
     int numberOfCows { 0 };
     cin >> numberOfPatches >> numberOfNhojCows >> numberOfCows;
     vector<pair<int, int>> spots;
-    long long maxTastiness { 0 };
     for (int i = 0; i < numberOfPatches; i++) {
-        int p;
-        int t;
-        cin >> p >> t;
-        maxTastiness += t;
-        spots.push_back({p, t});
+        int a, b;
+        cin >> a >> b;
+        spots.push_back({a, b});
     }
     for (int i = 0; i < numberOfNhojCows; i++) {
-        int f;
-        cin >> f;
-        spots.push_back({f, -1});
+        int a;
+        cin >> a;
+        spots.push_back({a, -1});
     }
     sort(spots.begin(), spots.end());
-    int total { numberOfPatches+numberOfNhojCows }; 
-    int lastCow { -1 };
-    vector<long long> tastiness;
-    for (int i = 0; i < total; i++) {
+    int size { numberOfNhojCows+numberOfPatches };
+    vector<long long> score;
+    int left { 0 };
+    int right { 1 };
+    long long between { 0 };
+    long long temp { 0 };
+    for (int i = 0; i < size; i++) {
         if (spots[i].second == -1) {
-            if (lastCow == -1) {
-                long long currentSum { 0 };
-                for (int j = 0; j < i; j++) {
-                    currentSum += spots[j].second;
-                }
-                tastiness.push_back(currentSum);
-            }
-            else {
-                long long radius { (spots[i].first-spots[lastCow].first) };
-                long long counter { 0 };
-                int k { lastCow+1 };
-                long long segment { 0 };
-                long long best { 0 };
-                for (int j = lastCow+1; j < i; j++) {
-                    counter += spots[j].second;
-                    segment += spots[j].second;
-                    while (static_cast<long long>(spots[j].first - spots[k].first)*2 >= radius) {
-                        counter -= spots[k].second;
-                        k++;
-                    }
-                    best = max(best, counter);
-                }
-                tastiness.push_back(best);
-                tastiness.push_back(segment-best);
-            }
-            lastCow = i;
-        }
-    }
-    long long count { 0 }; 
-    for (int i = total-1; i >= 0; i--) {
-        if (spots[i].second == -1) {
-            tastiness.push_back(count);
+            left = i;
+            right = i+1;
+            score.push_back(temp);
             break;
         }
+        temp += spots[i].second;
+    }
+    while (left < size-1 && right <= size-1) {
+        if (spots[right].second != -1) {
+            between += spots[right].second;
+            right++;
+        }
         else {
-            count += spots[i].second;
+            int radius { spots[right].first - spots[left].first };
+            int l { left+1 };
+            long long value { 0 };
+            long long cur { 0 };
+            for (int r = left+1; r < right; r++) {
+                cur += spots[r].second;
+                while ((spots[r].first - spots[l].first)*2 >= radius) {
+                    cur -= spots[l].second;
+                    l++;
+                }
+                value = max(value, cur);
+            }
+            score.push_back(value);
+            score.push_back(between-value);
+            between = 0;
+            left = right;
+            right++;
         }
     }
+    temp = 0;
+    for (int i = left+1; i < size; i++) {
+        temp += spots[i].second;
+    }
+    score.push_back(temp);
+    sort(score.begin(), score.end());
     long long answer { 0 };
-    sort(tastiness.begin(), tastiness.end());
-    reverse(tastiness.begin(), tastiness.end());
-    for (int i = 0; i < min(numberOfCows, static_cast<int>(tastiness.size())); i++) {
-        answer += tastiness[i]; 
+    for (int i = 0; i < min(numberOfCows, static_cast<int>(score.size())); i++) {
+        answer += score.back();
+        cout << score.back() << "\n";
+        score.pop_back();
     }
-    if (numberOfNhojCows == 0) {
-        cout << maxTastiness << "\n";
-    }
-    else {
-        cout << answer << "\n";
-    }
+    cout << answer << "\n";
 }
 
 int main() {
